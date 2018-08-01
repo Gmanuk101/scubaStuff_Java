95426770bf388f1187ea12f29e9b561238d785e5
Gmanuk101 <graemephillipsuk@gmail.com>
2018-08-01 09:15:14 +0100

    refactored code

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ package com.revision;
 import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
+import static com.revision.physics.DaltonsLaw.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -21,63 +23,11 @@ public class Main {
         System.out.println("Your ATM is: "+atm);
         System.out.println("Your total pressure is: "+dalton);
         System.out.println("Your Oxygen Part Pressure is: "+oxyPart);
-        System.out.println("Your Oxygen Part Pressure is: "+nitroPart);
+        System.out.println("Your Nirtorgen Part Pressure is: "+nitroPart);
 
 
     }
 
-    static public double daltonsLaw(double atm)
-    {
-        double oxy = 0.21;
-        double nit = 0.79;
-        double oxyCalc = atm * oxy;
-        double nitroCalc = atm * nit;
-        double dalton = (oxyCalc + nitroCalc);
-
-
-        return dalton;
-    }
-
-    static public double calcOxyPress(double atmosphere)
-    {
-        double oxy = 0.21;
-        double calc = oxy * atmosphere;
-        return calc;
-    }
-
-    static public double calcNitrPress(double atmosphere)
-    {
-        double nit = 0.79;
-        double calc = nit * atmosphere;
-        return calc;
-    }
-
-    static public int enterDepth(int depth)
-    {
-        int atmosphere = 0;
-
-        if(depth <10)
-        {
-            atmosphere = 1;
-        }
-        else if((depth >=10) && (depth <20))
-        {
-            atmosphere = 2;
-        }
-        else if((depth >=20) && (depth <30))
-        {
-            atmosphere =3;
-        }
-        else if((depth >=30) && (depth <40))
-        {
-            atmosphere =4;
-        }
-        else if((depth >=40) && (depth <50))
-        {
-            atmosphere=5;
-        }else atmosphere = 6;
-        return atmosphere;
-    }
 
 
 
